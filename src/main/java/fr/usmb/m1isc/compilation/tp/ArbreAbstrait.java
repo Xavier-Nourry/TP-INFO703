1e9c9e15e0eb737d92c17f45a64d45176f5834ac
Camille <43339411+Camille0907@users.noreply.github.com>
2020-12-10 17:48:38 +0100

    les instructions nécessaires au premier exemple sont générées

--- src/main/java/fr/usmb/m1isc/compilation/tp/ArbreAbstrait.java
+++ src/main/java/fr/usmb/m1isc/compilation/tp/ArbreAbstrait.java
@@ -44,12 +44,79 @@ public class ArbreAbstrait {
 
     public String toAsm(){
         String dataSegment = "DATA SEGMENT\n";
-        String codeSegment = " CODE SEGMENT\n";
+        String codeSegment = "CODE SEGMENT\n";
 
-        /* Y a plus qu'à !*/
+        //Génération des instructions
+        codeSegment += genereInstructions();
 
         dataSegment += "DATA ENDS\n";
         codeSegment += "CODE ENDS";
         return dataSegment + codeSegment;
+    }
+
+    private String genereInstructions() {
+        switch(valeur) {
+            case ";":
+                return InstructionsPointVirgule();
+            case "LET":
+                return InstructionsLet();
+            case "200":
+            case "119":
+            case "100":
+                return InstructionsInt();
+            case "/":
+                return InstructionsDivison();
+            case "*":
+                return InstructionsMultiplication();
+            default:
+                return InstructionVariable();
+        }
+    }
+
+    private String InstructionVariable() {
+        String res = "mov eax, " + valeur + "\n";
+        res += "push eax\n";
+        return res;
+    }
+
+    private String InstructionsMultiplication() {
+        String res = fils1.genereInstructions();
+        res += fils2.genereInstructions();
+        res += "pop ebx\n";
+        res += "pop eax\n";
+        res += "mul eax, ebx\n";
+        res += "push eax\n";
+        return res;
+    }
+
+    private String InstructionsDivison() {
+        String res = fils1.genereInstructions();
+        res += fils2.genereInstructions();
+        res += "pop ebx\n";
+        res += "pop eax\n";
+        res += "div eax, ebx\n";
+        res += "push eax\n";
+        return res;
+    }
+
+    private String InstructionsInt() {
+        String res = "mov eax, " + valeur + "\n";
+        res += "push eax\n";
+        return res;
+    }
+
+    private String InstructionsLet() {
+        String res = fils2.genereInstructions();
+        res += "pop eax\n";
+        res += "mov " + fils1.valeur + ", eax\n";
+        res += "push eax\n";
+        return res;
+    }
+
+    private String InstructionsPointVirgule() {
+        String res = fils1.genereInstructions();
+        res += "pop eax\n";
+        res += fils2.genereInstructions();
+        return res;
     }
 }
