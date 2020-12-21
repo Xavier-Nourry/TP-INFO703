ca6654ca5bfa44219b4c0b39a9dc416bcb885058
Camille <43339411+Camille0907@users.noreply.github.com>
2020-12-21 17:00:22 +0100

    adaptation de l'écriture dans un fichier

--- src/main/java/fr/usmb/m1isc/compilation/tp/outils_fichier/FileTools.java
+++ src/main/java/fr/usmb/m1isc/compilation/tp/outils_fichier/FileTools.java
@@ -3,7 +3,7 @@ package fr.usmb.m1isc.compilation.tp.outils_fichier;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class FileTools { //TODO : cette classe devrait être static mais l'IDE ne veut pas ¯\_('_')_/¯
+public class FileTools {
     public static void writeStringIntoFile(String fileName, String toWrite){
         try {
             FileWriter myWriter = new FileWriter("./" + fileName);
